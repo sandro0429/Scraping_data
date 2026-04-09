19b59fec4d98b0bf17e8f70b0102bfb980c3e9a8
Sandro Sotacuro Escobar <s.sotacuroe@alum.up.edu.pe>
2026-04-09 03:38:30 -0500

    updade homework
    I created the environment to start programming

--- scraper.py
+++ scraper.py
@@ -1,1 +1,13 @@
-# Main extraction script
+# TAREA N°2
+import pandas as pd
+import time
+
+# Herramientas de selenium
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
+from webdriver_manager.chrome import ChromeDriverManager
